4da7f9774f92a1039f0e156490747d068adb9c87
Bledi Piperku <Bledip2003@gmail.com>
2024-04-18 15:59:41 +0200

    project configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,14 +7,17 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    colors: {
+      primary: "#ff0000",
+      white: "#ffffff",
+      black: "#000000",
     },
+    fontFamily: {
+      sans: ["Inter", "sans-serif"],
+    },
+    extend: {},
   },
   plugins: [],
 };
+
 export default config;
