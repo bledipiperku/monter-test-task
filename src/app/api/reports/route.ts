0861157bfa28f6157c24fd25be035af629b93e92
Bledi Piperku <Bledip2003@gmail.com>
2024-04-19 04:54:08 +0200

    error fix

--- src/app/api/reports/route.ts
+++ src/app/api/reports/route.ts
@@ -1,13 +1,9 @@
 import { NextRequest, NextResponse } from "next/server";
 
 const BASE_URL = "https://jsonplaceholder.typicode.com"; // This is a dummy API
-const CACHE_EXPIRATION = 30 * 24 * 60 * 60 * 1000; // 30 days in milliseconds
-
-// In-memory cache for place details
-const cache = new Map<string, any>();
 
 function isValidPageNumber(pgNumber: string | null): boolean {
-  // Validate pgNumber (e.g., ensure it's a positive integer)
+  // Validate pgNumber is a positive integer
   return pgNumber !== null && /^\d+$/.test(pgNumber) && parseInt(pgNumber) > 0;
 }
 
@@ -34,19 +30,7 @@ export async function GET(request: NextRequest) {
     pgNumber = pgNumber ?? "1";
     pgSize = pgSize && /^\d+$/.test(pgSize) ? pgSize : "10";
 
-    const cacheKey = `${pgNumber}-${pgSize}`;
-
-    // Check if the data is present in the cache
-    if (cache.has(cacheKey)) {
-      return NextResponse.json({ reportDetails: cache.get(cacheKey) });
-    }
-
-    // Fetch data if not in cache
     const reportDetails = await fetchData(pgNumber, pgSize);
-
-    // Store the fetched data in the cache
-    cache.set(cacheKey, reportDetails);
-    setTimeout(() => cache.delete(cacheKey), CACHE_EXPIRATION); // Cache expiration
 
     return NextResponse.json({ reportDetails });
   } catch (error) {
